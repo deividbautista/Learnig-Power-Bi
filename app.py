e06e7a45bc6fbc07dc27dedabd8efc65b79475a8
deividbautista <debautistaocampo@gmail.com>
2025-07-07 22:19:27 -0500

    Initial commit for Luminine Data Center project

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import pandas as pd
+
+# URL del API
+url = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=1000"
+
+# Hacemos la petición
+response = requests.get(url)
+
+# Convertimos la respuesta JSON a un diccionario
+data = response.json()
+
+records = data[1]
+
+df = pd.json_normalize(records)
+df = df[["country.id", "country.value", "value", "date"]]
+df.columns=["pais_id", "nombre_pais", "poblacion", "año"]
+df.dropna(inplace=True)
+
+df.to_json("poblacion.json", orient="records")
