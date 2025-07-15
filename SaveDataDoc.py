58e52f1f1474892ad4d28b43fc984234d5564570
Jhonatan2022 <florezj328@gmail.com>
2025-07-14 22:55:59 -0500

    Save data file

---
+++ SaveDataDoc.py
@@ -0,0 +1,16 @@
+import os
+
+
+def savDocData(df, folderName, fileName):
+
+    if df.empty:
+        print("Data frame empty")
+
+    else:
+        if not os.path.exists(folderName):
+            # Creamos la carpeta
+            os.makedirs(folderName)
+
+        # Usamos la ruta de la carpeta y el nombre del archivo
+        filePath = os.path.join(folderName, fileName)
+        df.to_json(filePath, orient="records")
